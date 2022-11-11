95b854d401808412ac3a3b5e5691f4c752286548
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-11-11 12:46:55 +0800

    Text Updates

--- maint/settings.py
+++ maint/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'maint.middleware.CacheControl',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'maint.urls'
