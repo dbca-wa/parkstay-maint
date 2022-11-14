82026185b558fbbae837fc18ec14ede30cede70f
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-11-14 15:20:19 +0800

    404 redirect to homepage to capture direct page links to parkstay

--- maint/settings.py
+++ maint/settings.py
@@ -59,7 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'maint.middleware.CacheControl',
+#    'maint.middleware.CacheControl',
+    'maint.middleware.RedirectUrls',
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
